6d0ddf9a4c69708e0d46e5297fa8e59019f10ff3
Ale <alexcoppe@gmail.com>
2016-01-20 23:04:55 +0100

    Added the setUp method to FeedTest class

--- tests.py
+++ tests.py
@@ -5,19 +5,18 @@ from feedoproxy.Feed import Feed
 
 class FeedTest(unittest.TestCase):
 
+    def setUp(self):
+        self.feed_string = open("test-data/avax.xml").read()
+
 
     def test_can_read_a_feed_in_a_string(self):
-        feed_string = open("test-data/avax.xml").read()
-
-        feed = Feed(feed_string) 
+        feed = Feed(self.feed_string) 
         
-        self.assertEqual(feed_string, feed.feed_string)
+        self.assertEqual(self.feed_string, feed.feed_string)
 
 
     def test_feed_items_method_returns_right_number_of_items(self):
-        feed_string = open("test-data/avax.xml").read()
-
-        feed = Feed(feed_string)
+        feed = Feed(self.feed_string)
 
         self.assertEqual(20, len(feed.items()))
 
